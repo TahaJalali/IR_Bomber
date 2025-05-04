4b477ffcbaee43a39465cc50eb273c7bea66f84f
Taha Jalali <TahaJalali92@gmail.com>
2025-05-04 21:21:52 +0330

    add Flask app with HTML button for database insertion

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/InsertDB")
+def hello_world():
+    # Create a simple button in cernter of the page in html
+    return """<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Insert Api DB</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        button {
+            padding: 10px 20px;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+    <button onclick='alert("DB Insterted Sucsessfuly!")'>Insert DB</button>
+</body>
+</html>"""
+app.run()
+
 def Main():
     # Code to handle the main logic of the program
     pass
