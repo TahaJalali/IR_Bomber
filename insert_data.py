5734e45125a35699289b974aceaadc4f58435f19
Taha Jalali <TahaJalali92@gmail.com>
2025-05-08 17:24:27 +0330

    add database connection and table drop function in insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -1,9 +1,16 @@
+import sqlite3
+
 def DB_Connection():
     # Code to establish a connection to the sqlite database
+    Connection = sqlite3.connect('api_list.db')
     pass
 
 def DB_Create_Table():
     # Code to create a table in the sqlite database
+    pass
+
+def DB_Table_Drop():
+    # Code to drop the table in the sqlite database
     pass
 
 def DB_Insert_Data():
